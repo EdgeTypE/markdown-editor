7ca186b5a8447334fe63407922145af354757ad0
EdgeTypE <minefpsgelistirici@gmail.com>
2024-05-07 11:59:02 +0300

    önizleme

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function applyMarkdown(action) {
     }
 
     editor.value = editor.value.substring(0, start) + markdownText + editor.value.substring(end);
+    updatePreview();
 }
 
 function insertImage() {
@@ -31,5 +32,29 @@ function insertImage() {
         const imageMarkdown = `![Alt text](${imageUrl})`;
 
         editor.value = editor.value.substring(0, start) + imageMarkdown + editor.value.substring(start);
+        updatePreview();
     }
+}
+
+function updatePreview() {
+    const editor = document.getElementById("markdown-editor");
+    const preview = document.getElementById("markdown-preview");
+
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(editor.value);
+    preview.innerHTML = html;
+}
+
+function downloadMarkdown() {
+    const editor = document.getElementById("markdown-editor");
+    const markdownContent = editor.value;
+    const blob = new Blob([markdownContent], { type: 'text/markdown' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'document.md';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 }
